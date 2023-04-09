aa422afee134b8bc68add7eb1f184dc838f6d97a
Jichun Wu <jromwu@gmail.com>
2023-04-09 16:29:31 +0100

    fix environment variable in deployment script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for (( i=0; i<$1; i++ )); { sudo docker compose build && sudo docker compose up --force-recreate --remove-orphans --abort-on-container-exit --exit-code-from=client; }
+for (( i=0; i<$1; i++ )); { export TARGET="$TARGET" CHROME_SETUP="$CHROME_SETUP"; sudo docker compose build && sudo docker compose up --force-recreate --remove-orphans --abort-on-container-exit --exit-code-from=client; }
