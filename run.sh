1731ffe9f681b50bf019b5c5a8a65e922612760d
Jichun Wu <jromwu@gmail.com>
2023-04-10 11:06:10 +0100

    add target to deployment script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for (( i=0; i<$1; i++ )); { sudo docker compose build && sudo TARGET="$TARGET" CHROME_SETUP="$CHROME_SETUP" docker compose up --force-recreate --remove-orphans --abort-on-container-exit --exit-code-from=client; }
+for (( i=0; i<$2; i++ )); { sudo docker compose build && sudo TARGET="$1" CHROME_SETUP="$CHROME_SETUP" docker compose up --force-recreate --remove-orphans --abort-on-container-exit --exit-code-from=client; }
